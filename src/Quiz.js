84b1878b595126ec223e89b8ca49cfe1e5090044
azurizott <39562124+azurizott@users.noreply.github.com>
2018-06-21 16:19:54 -0400

    step 4 (again)

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,7 +14,7 @@ class Quiz extends Component {
         
         return (
             <div>
-                { this.isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={ quizData.quiz_questions[this.state.quiz_position - 1] } /> }
+                { isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={ quizData.quiz_questions[this.state.quiz_position - 1] } /> }
             </div>
         )
     }
